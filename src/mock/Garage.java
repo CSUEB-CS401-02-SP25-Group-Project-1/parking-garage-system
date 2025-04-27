7b0eac80e150763d161188ac4c9adbab6d634b2c
muffindelegate39423 <123130275+muffindelegate39423@users.noreply.github.com>
2025-04-26 21:28:15 -0700

    wip garage mockup

--- src/mock/Garage.java
+++ src/mock/Garage.java
@@ -17,73 +17,89 @@ public class Garage {
 	
 	// Constructors
 	public Garage() {
-		
+		id = "GA"+count++;
 	}
 	
 	public Garage(int capacity, double hourlyRate, String name) {
-		
+		id = "GA"+count++;
 	}
 	
 	// Getters & Setters
-	public String getId() {
-		
+	public String getID() {
+		return id;
 	}
+	
 	public String getName() {
-		
+		return name;
 	}
+	
 	public void setName(String name) {
-		
+		this.name = name;
 	}
 	
 	public int getCapacity() {
-		
-	}
-	public void setCapacity(int capacity) {
-		
+		return capacity;
 	}
 	
-	public double getHourlyRate() {
-		
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
 	}
-	public void setHourlyRate(double newRate) {
-		
+	
+	public double getHourlyRate() { // returns fixed rate of garage (not to be confused with hourly earnings)
+		return hourlyRate;
+	}
+	
+	public void setHourlyRate(double newRate) { // sets new fixed rate for garage (not to be confused with hourly earnings)
+		this.hourlyRate = newRate;
 	}
 	
 	public int getAvailableSpaces() {
-		
+		return (capacity - activeTickets.size());
 	}
+	
 	public Boolean isFull() {
-
+		return (capacity == activeTickets.size());
 	}
 	
 	// Ticketing
-	public String generateTicket() {
+	public String generateTicket() { // adds new ticket to both ticket lists
 
 	}
-	public Receipt payTicket(String ticketId, double amount) {
+	
+	public Receipt payTicket(String ticketID, double amount) { // removes ticket from activeTicket list
 		
 	}
 	
 	// Reporting
-	public Report viewReport(Usertype requester) {
-		
+	public Report viewReport(UserType requester) {
+		// TODO: make dummy report
 	}
+	
 	public double getTotalRevenue() {
-		
+		return 420.07; // dummy value
 	}
 	
 	// Security Cameras
 	public boolean addCamera(SecurityCamera cam) {
-		
+		cams.add(cam);
 	}
-	public boolean removeCamera(String cameraId) {
-		
-	}
-	public SecurityCamera[] getCameras()
-	{
+	
+	public boolean removeCamera(String cameraID) {
 		
 	}
 	
+	public ArrayList<SecurityCameras> getCameras()
+	{
+		return cams;
+	}
+	
 	// Misc
+	public void addLogEntry(String logString) { // adds log entry (as a string) to garage
+		logEntries.add(logString);
+	}
+	
+	public ArrayList<String> getLogEntries() {
+		return logEntries;
+	}
 }
 
