42fa61002e1be79057dffa94c301166262285f30
ninja132441 <157433292+ninja132441@users.noreply.github.com>
2025-04-27 21:28:16 -0700

    Update Employee.java
    Added the methods that matches the server requirement

--- src/Employee.java
+++ src/Employee.java
@@ -1,24 +1,55 @@
-public class Employee {
-    private String id;
-    private String username;
-    private String password;
-    private Garage garage;
+package mock;
 
-    // Constructor
-    public Employee(Garage garage) {
-        this.garage = garage;
-    }
+import java.util.Date;
+import interfaces.EmployeeInterface;
 
-    // Getter methods
-    public String getID() {
-        return id;
-    }
+public class Employee extends User implements EmployeeInterface {
+	private static int count = 0;
+	private String id;
+	private String username;
+	private String password;
+	
+	public Employee(Garage garage, String username, String password) {
+		id = "EM"+count++;
+		this.username = username;
+		this.password = password;
+		this.garage = garage;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getID() {
+		return id;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String newPassword) {
+		password = newPassword;
+	}
+
+	public boolean overrideTicketFee(String ticketID, double newFee) {
+		// returns true if ticket was found and was overridden
+		// returns false otherwise
+		return false; // dummy value
+	}
+
+	public Receipt getReceipt(String ticketID) {
+		// returns the receipt of a ticket
+		return new Receipt("TI600", "The Awesome Garage", new Date(), new Date(), 79.99); // dummy value
+	}
+
+	public Report getGarageReport() {
+		// returns a generated report of the garage
+		return new Report(); // dummy value
+	}
+
+	public void setGarageHourlyRate(double newRate) {
+		garage.setHourlyRate(newRate);
+	}
+
 }
