932c1d87e4509aad7e9d5dd85496ee034947246a
Riley Guidry <rguidry2@horizon.csueastbay.edu>
2025-04-28 19:42:19 -0700

    Fixed userType assigning

--- src/Customer.java
+++ src/Customer.java
@@ -4,10 +4,12 @@ public class Customer extends User implements CustomerInterface{
 
 	public Customer(Garage garage) {
 		this.garage = garage;
+		userType = UserType.Customer;
 	}
 
 	public Customer() { // no arguments constructor for testing
-		garage = null;
+		garage = new Garage();
+		userType = UserType.Customer;
 	}
 
 	// most methods a Customer will use come from the parent class User
