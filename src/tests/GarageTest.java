c11daa6a8fe25328d425b5b2580cc7955408a10f
Riley Guidry <rguidry2@horizon.csueastbay.edu>
2025-04-28 19:23:50 -0700

    Made GarageTest class

---
+++ src/tests/GarageTest.java
@@ -0,0 +1,74 @@
+package tests;
+
+import mock.*;
+
+import org.junit.*;
+
+public class GarageTest {
+	
+	@Test
+	public void testSetters() {
+		String name = "name";
+		int capacity = 200;
+		double rate = 20.25;
+		Garage garage1;
+		garage1.setName(name);
+		garage1.setCapacity(capacity);
+		garage1.setHourlyRate(rate);
+
+		Garage garage2 = new Garage(name, capacity, rate);
+	
+		// if ids are unequal
+		assertTrue(garage1.getID().compareTo(garage2.getID()) != 0);
+
+		// if setter set name properly
+		assertEquals(name, garage1.getName());
+		assertEquals(name, garage2.getName());
+
+		// if setter set capacity properly
+		assertEquals(capacity, garage1.getCapacity());
+		assertEquals(capacity, garage2.getCapacity());
+
+		// if setter set rate properly
+		assertEquals(rate, garage1.getHourlyRate());
+		assertEquals(rate, garage2.getHourlyRate());
+	}
+
+	@Test
+	public void testTickets() {
+		String name = "name";
+		int capacity = 20;
+		double rate = 10.50;
+		Garage garage = new Garage(name, capacity, rate);
+
+		List<Ticket> buffer = new ArrayList<>();
+		
+		// fill garage with tickets
+		// catch returned tickets in a buffer to test more getters
+		for (int i = 0; i < capacity; i++) {
+			buffer.add(garage.generateTicket());
+		}
+
+		// if any tickets have been added at all
+		assertTrue(garage.getTicket(0) != null);
+
+		// test if garage has been filled
+		assertTrue(garage.isFull());
+
+		// if the returned ticket is accurate
+		assertEquals(buffer.get(0), garage.getTicket(0));
+		// and a few others
+		assertEquals(buffer.get(5), garage.getTicket(5));
+		assertEquals(buffer.get(10), garage.getTicket(10));
+		assertEquals(buffer.get(15), garage.getTicket(15));
+
+
+		// test payTicket() by paying off every ticket
+		for (Ticket ticket : garage.getTickets()) {
+			garage.payTicket(ticket.getID(), 4000); // big number
+		}
+
+		// test if garage is no longer full
+		assertTrue(!garage.isFull());
+	}
+}
