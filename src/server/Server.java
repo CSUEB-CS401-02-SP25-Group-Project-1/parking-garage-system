108954ae99869bc290765ad567074c0432c27982
muffindelegate39423 <123130275+muffindelegate39423@users.noreply.github.com>
2025-04-29 20:42:39 -0700

    created gatehandler class

--- src/server/Server.java
+++ src/server/Server.java
@@ -396,6 +396,39 @@ public class Server {
 			// return live feed in a message
 		}
 		
-		
+		private class GateHandler implements Runnable { // a class that keeps gate toggling logic running in another thread
+			private Gate gate;
+			private Log log;
+			
+			public GateHandler(Gate gate, Log log) {
+				this.gate = gate;
+				this.log = log;
+			}
+			
+			public void run() {
+				if (gate.isOpen()) {
+					closeGate();
+				} else {
+					openGate();
+				}
+			}
+			
+			private void openGate() { // opens gate for a certain amount of time before automatically closing it
+				gate.open();
+				log.append(LogType.ACTION, "Gate is now opening in garage "+garage.getID(), garage);
+				long millis = (long)(gate.getOpenTime() * 1000); // remains open for this amount of time before automatically closing
+				try {
+					Thread.sleep(millis); // wait
+					closeGate();
+				} catch (InterruptedException e) {
+					log.append(LogType.ERROR, "Unable to close gate in garage "+garage.getID(), garage);
+				} 
+			}
+			
+			private void closeGate() {
+				gate.close();
+				log.append(LogType.ACTION, "Gate is now closing in garage "+garage.getID(), garage);
+			}
+		}
 	}
 }
