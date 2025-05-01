0c52c7e7e8568195826b0feb862a5f17e797e0ad
muffindelegate39423 <123130275+muffindelegate39423@users.noreply.github.com>
2025-04-30 18:45:52 -0700

    Create ServerTest.java

---
+++ src/tests/ServerTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ServerTest { // server must be ran first before running these test clients
+	@Test
+	public void CustomerInteractionTest() {
+		
+	}
+	
+	@Test
+	public void EmployeeInteractionTest() {
+		
+	}
+}
