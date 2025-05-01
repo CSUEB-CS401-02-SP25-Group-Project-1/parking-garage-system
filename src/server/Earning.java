868468286daab9f19e396cea6045c18ceaf163c1
muffindelegate39423 <123130275+muffindelegate39423@users.noreply.github.com>
2025-05-01 02:40:27 -0700

    tested earning class

---
+++ src/server/Earning.java
@@ -0,0 +1,26 @@
+package server;
+
+import java.util.Date;
+import interfaces.EarningInterface;
+
+public class Earning implements EarningInterface {
+	private Date date;
+	private double revenue;
+	
+	public Earning(Date date, double revenue) {
+		this.date = date;
+		this.revenue = revenue;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public double getRevenue() {
+		return revenue;
+	}
+	
+	public String toString() {
+		return date.getTime()+","+revenue;
+	}
+}
