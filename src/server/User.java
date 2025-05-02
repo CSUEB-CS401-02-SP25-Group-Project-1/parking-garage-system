bb2c547bef14dac0e3ec8f451f5b75fe4cf04e7a
PaulGMedina <157787235+PaulGMedina@users.noreply.github.com>
2025-05-02 15:32:43 -0700

    Update User.java
    update needs testing and any error corrections.

--- src/server/User.java
+++ src/server/User.java
@@ -2,7 +2,7 @@ package server;
 
 import interfaces.UserInterface;
 
-public class User implements UserInterface {
+public class User implements UserInterface{
 	protected Garage garage;
 	protected UserType userType; 
 	
@@ -16,30 +16,69 @@ public class User implements UserInterface {
 		this.userType = UserType.Undefined;
 	}
 	
-	public void setGarage(Garage garage){
-		this.garage = garage;
-	}
-	
-	public Garage getGarage() {
-		return this.garage;
-	}
-	
-	public UserType getType() {
-		return this.userType;
-	}
+	public void setGarage(Garage garage){this.garage = garage;}
+	public Garage getGarage() {return this.garage;}
+	public UserType getType() {return this.userType;}
 
-	public String generateTicket() {	
-		return garage.generateTicket();
+	
+	public Receipt payTicket(String ticketID) {		//ticket
+		Ticket ticket = garage.getTicket(ticketID);
+		ticket.pay(ticket.getFee());
+		Receipt receipt = new Receipt(ticket, ticket.getFee());
+		System.out.println(" Parking Fee Total: " + ticket.toString() + "\n Ticket Successfully Paid");
+		return receipt;
 	}
 	
-	public Receipt payTicket(String ticketID, double paymentAmount) {	
-		return garage.payTicket(ticketID, paymentAmount);
+	public void printReceipt(Ticket ticket){
+		Ticket tempTicket = garage.getTicket(ticket.getID());
+		if(tempTicket != null && tempTicket.isPaid() == true) {
+			Receipt receipt = new Receipt(tempTicket, ticket.getFee());
+			System.out.println("Receipt Total: \n" + receipt.toString());
+		}
 	}
-
+	
+	public String generateTicket() {		//garage and ticket
+		String ticketID = garage.generateTicket();
+		if(ticketID != null) {
+			return "Ticket has been generated.\n Ticket ID: " + ticketID;
+		}
+		return null;
+	}
+	
 	public int getAvailableSpaces() {
-		return garage.getAvailableSpaces();
+		int currentAvailableSpots = this.garage.getCapacity();
+		return currentAvailableSpots;
 	}
-
+	
+	public Receipt payTicket1(String ticketID) {  
+		if(this.garage != null) {
+			Ticket ticket = this.garage.getTicket(ticketID);
+			Receipt receipt = new Receipt(ticketID, getGarageName(), ticket.getEntryTime(), ticket.getExitTime(), (garage.getHourlyRate() * 4));
+			receipt.toString();
+			return receipt;
+		}
+		return null;
+	}
+	
+	public String getGateStatus() {
+		if(this.garage != null) {
+			Gate gate = this.garage.getGate();
+			if(gate.open)
+				return "Gate is opened";
+			else
+				return "Gate is closed";
+		}
+		return null;
+	}
+	
+	public void toggleGate() {
+		if(this.garage != null) {
+			Gate gate = this.garage.getGate();
+			Log serverLog = new Log();
+			new Thread(new GateHandler(gate, serverLog)).start();
+		}
+	}
+	
 	public String viewBillingSummary(String ticketID) {
 		//Find ticket
 		Ticket ticket = garage.getTicket(ticketID);
@@ -51,19 +90,29 @@ public class User implements UserInterface {
 					ticket.getExitTime() + ":" +
 					ticket.getFee();
 		else
+			System.out.println("Ticket not found!");
 		//If ticket was NOT found, return null
 		return null;
 	}
-
+	
 	public String getGarageName() {
-		return garage.getName();
+		String garageName = null;
+		if(this.garage != null) {
+			garageName = this.garage.getName();
+		}
+		return garageName;
 	}
 
-	public void toggleGate() {
-		Gate gate = garage.getGate();
-		if(gate.isOpen())
-			gate.close();
-		else
-			gate.open();
+	@Override
+	public Receipt payTicket(String ticketID, double paymentAmount){ 
+		if(this.garage != null) {
+			Ticket ticket = this.garage.getTicket(ticketID);
+			Receipt receipt = new Receipt(ticketID, getGarageName(), 
+					ticket.getEntryTime(), ticket.getExitTime(), ticket.getFee());
+			receipt.toString();
+			
+			return receipt;
+		}
+		return null;
 	}
 }
