d1e39cca27ad2bd7f06c3e0699afa2e2257972d7
muffindelegate39423 <123130275+muffindelegate39423@users.noreply.github.com>
2025-05-01 19:49:57 -0700

    finished image message test

--- src/tests/ImageMessageTest.java
+++ src/tests/ImageMessageTest.java
@@ -1,5 +1,16 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
+import java.nio.file.Paths;
+import javax.swing.ImageIcon;
+import org.junit.Test;
+import shared.ImageMessage;
+
 public class ImageMessageTest {
-    
+    @Test
+    public void ImageTest() {
+        ImageIcon image = new ImageIcon(Paths.get("live-feed", "feed_1.jpg").toString());
+        ImageMessage temp = new ImageMessage(null, null, null, image);
+        assertTrue(temp.getImage().equals(image));
+    }
 }
