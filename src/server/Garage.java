8b3ac5dbc210282b2cb1a5e2028ec8ea462089ea
Isaiah White <119544336+isaiahw13@users.noreply.github.com>
2025-05-01 20:25:02 -0700

    Move garage and receipt to /src/server
    Moved Garage and Receipt into the server folder that they belong in

---
+++ src/server/Garage.java
@@ -0,0 +1,167 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.Date;
+import interfaces.GarageInterface;
+
+public class Garage implements GarageInterface {
+	private static int count = 0;
+	private String id;
+	private String name;
+	private double hourlyRate;
+	private int capacity;
+	private ArrayList<Ticket> allTickets = new ArrayList<>(); // every ticket ever associated with garage
+	private ArrayList<Ticket> activeTickets = new ArrayList<>(); // currently parked vehicles
+	private ArrayList<SecurityCamera> cameras = new ArrayList<>(); // list of cameras associated with garage
+	private ArrayList<String> logEntries = new ArrayList<>(); // every log entry related to garage for employees to view (since boot-up)
+	private Gate gate;
+	private Report report; // add every new entry and exit to report
+	
+	public Garage(String name, double hourlyRate, int capacity, double gateOpenTime) {
+		id = "GA"+count++;
+		this.name = name;
+		this.hourlyRate = hourlyRate;
+		this.capacity = capacity;
+		gate = new Gate(gateOpenTime);
+	}
+
+	public String getID() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String newName) {
+		name = newName;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public double getHourlyRate() {
+		return hourlyRate;
+	}
+
+	public void setHourlyRate(double newRate) {
+		hourlyRate = newRate;
+	}
+
+	public int getAvailableSpaces() {
+		return (capacity - activeTickets.size());
+	}
+
+	public boolean isFull() {
+		return (capacity == activeTickets.size());
+	}
+
+	public String generateTicket() { // returns ticket ID if successful
+		if (!isFull()) {
+			Ticket ticket = new Ticket(this);
+			allTickets.add(ticket);
+			activeTickets.add(ticket);
+			return ticket.getID();
+		}
+		return null;
+	}
+	
+	@Override
+	public void loadTicket(Ticket ticket) { // used by server to load existing tickets to both arrays
+		if (!ticket.isPaid()) {
+			activeTickets.add(ticket);
+		}
+		allTickets.add(ticket);
+	}
+	
+	public Ticket getTicket(String ticketID) { // returns ticket from all tickets list based on its ticket id
+		for(int i = 0; i < this.allTickets.size(); i++) {
+			if(this.allTickets.get(i).getID().equalsIgnoreCase(ticketID))
+				return this.allTickets.get(i);
+		}
+		return null;
+	}
+
+	public Receipt payTicket(String ticketID) { // returns receipt if successful
+		//Find ticket by ID
+		Ticket ticket = getTicket(ticketID);
+		
+		//Return null if the ticket is already paid
+		if(ticket.isPaid()) {
+			return null;
+		}
+		
+		//Mark ticket paid and remove from active tickets list
+		ticket.pay();
+		this.activeTickets.remove(ticket);
+		
+		//Create and return new receipt of ticket payment
+		return new Receipt(ticket);
+	}
+	
+	@Override
+	public void loadReport(Report report) { // used by server to load report from file
+		this.report = report;
+	}
+
+	public Report viewReport() {
+		return this.report;
+	}
+
+	@Override
+	public boolean addCamera(SecurityCamera newCamera) {
+		// add camera to garage's camera list if camera hasn't been added yet
+		int camCount = this.cameras.size();
+		cameras.add(newCamera);
+		
+		// return true if added successfully
+		if(cameras.size() > camCount)
+			return true;
+		
+		// return false if camera couldn't be added (duplicate cameras)
+		return false;
+	}
+
+	public boolean removeCamera(String cameraID) {
+		// remove camera based on its camera ID
+		for(int i = 0; i < this.cameras.size(); i++) {
+			// return true if camera was successfully removed
+			if(this.cameras.get(i).getID().equalsIgnoreCase(cameraID)) {
+				this.cameras.remove(i);
+				return true;
+			}
+		}
+	
+		// return false if no camera with such ID was found
+		return true; // dummy value
+	}
+
+	public ArrayList<SecurityCamera> getCameras() {
+		return cameras;
+	}
+
+	public void addLogEntry(String logEntry) {
+		logEntries.add(logEntry);
+	}
+
+	public ArrayList<String> getLogEntries() {
+		return logEntries;
+	}
+	
+	public Gate getGate() {
+		return gate;
+	}
+	
+	public String toString() {
+		return name+","+hourlyRate+","+capacity+","+gate.getOpenTime();
+	}
+
+	public ArrayList<Ticket> getActiveTickets() {
+		return activeTickets;
+	}
+
+	public ArrayList<Ticket> getAllTickets() {
+		return allTickets;
+	}
+}
