765772dfaa23cf2a79c40accf11882bde31c01d8
Riley Guidry <rguidry2@horizon.csueastbay.edu>
2025-05-03 19:19:52 -0700

    simple gate test

---
+++ src/tests/GateTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import server.Gate;
+import server.Garage;
+
+public class GateTest {
+    // test constructor
+    // test getters
+	
+    @Test
+    public void testGate() {
+	Garage g = new Garage();
+	double openTime = 20.0;
+        Gate gate = new Gate(g, openTime);
+	
+	assertEquals(g, gate.getGarage());
+	assertEquals(openTime, gate.getOpenTime());
+
+	openTime = 25.0;
+	gate.setOpenTime(openTime);
+	assertEquals(openTime, get.getOpenTime());
+
+	gate.close();
+	assertFalse(gate.isOpen());
+
+	gate.open();
+	assertTrue(gate.isOpen());
+    }
+}
